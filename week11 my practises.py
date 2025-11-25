a644b7eaf593770cc121affc85965eb4bd5e5574
ElifArpa <elifarpa1337@gmail.com>
2025-11-25 21:12:13 +0300

    Update week11 my practises.py

--- week11 my practises.py
+++ week11 my practises.py
@@ -1,7 +1,8 @@
-s = "   Hello World    "
+s = "   Hello World"
 #print(str.ljust(s,14,"!"))
 #print(str.rjust(s,14,"*"))
 #print(s.index("World"))
 #print(s.count("Hello"))
 #print(s.replace("World","Python"))
-print(str.strip(s))
+#print(str.strip(s))
+print(s.rstrip("d"))
