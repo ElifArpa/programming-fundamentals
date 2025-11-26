52b62f4c33d876cb7c8501df8165adaef1c95232
ElifArpa <elifarpa1337@gmail.com>
2025-11-26 22:50:00 +0300

    Update week11 my practises.py

--- week11 my practises.py
+++ week11 my practises.py
@@ -8,13 +8,41 @@
 #print(s.rstrip("d"))
 
 
-vowels = ["a","i","e","u","ü","o","ö"]
-def count_vowels(s):
-    vowel = 0
-    for letter in count_vowels:
-        if letter == vowels:
-            return vowel
+
+# def count_vowels(s):
+#     vowels = ["e","u","o","ü","a","i","ö"]
+#     vowel = 0
+#     for letter in s:
+#         if letter in vowels:
+#             vowel += 1
+#     return vowel
+# print(count_vowels("hello world"))
+
+
+
+# def sum_of_digit(s):
+#     sum = 0
+#     for digit in s:
+#         if digit.isdigit():
+#             sum += int(digit)
+#     return sum
+# print(sum_of_digit("ax3g5jd6"))
+
+
+
+# words = ["hello","world","my","name","is","Elif"]
+# total = 0
+# for word in words:
+#     if len(word) == 5:
+#         total +=1
+# print(total)
+
+
+sentence = "hello world my name is Elif"
+total =0
+for word in sentence:
+    if sentence.isalpha():
+        total +=1
     
-print(count_vowels("Hello,World"))
-
-
+print(len(0))
+    
