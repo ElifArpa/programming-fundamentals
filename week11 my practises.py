ac8888d08334e83b5ed9003ae5c5d24908a267a0
ElifArpa <elifarpa1337@gmail.com>
2025-11-26 20:01:14 +0300

    Update week11 my practises.py

--- week11 my practises.py
+++ week11 my practises.py
@@ -1,8 +1,20 @@
-s = "   Hello World"
+#s = "   Hello World"
 #print(str.ljust(s,14,"!"))
 #print(str.rjust(s,14,"*"))
 #print(s.index("World"))
 #print(s.count("Hello"))
 #print(s.replace("World","Python"))
 #print(str.strip(s))
-print(s.rstrip("d"))
+#print(s.rstrip("d"))
+
+
+vowels = ["a","i","e","u","ü","o","ö"]
+def count_vowels(s):
+    vowel = 0
+    for letter in count_vowels:
+        if letter == vowels:
+            return vowel
+    
+print(count_vowels("Hello,World"))
+
+
